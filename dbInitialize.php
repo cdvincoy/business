948a4febe2a40b9bdec8e6218d0d0c979b1806b1
cdvincoy <99915734+cdvincoy@users.noreply.github.com>
2025-05-30 09:50:31 +0800

    Final Update dbInitialize.php

--- dbInitialize.php
+++ dbInitialize.php
@@ -1,13 +1,14 @@
-<?php
+<?php // This file initializes the database.
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "business";
 
-
+// Create database
 $conn = new mysqli($servername,$username,$password);
 $sql = "CREATE DATABASE $dbname";
 
+// Check if database is created
 if($conn->query($sql) === TRUE){
     echo "Database created successfully";
     $conn->close();
