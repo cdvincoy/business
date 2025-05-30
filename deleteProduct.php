9c38885386192a052ebe1e89fd1376653b142873
cdvincoy <99915734+cdvincoy@users.noreply.github.com>
2025-05-30 10:56:54 +0800

    Final Update deleteProduct.php

--- deleteProduct.php
+++ deleteProduct.php
@@ -1,10 +1,13 @@
-<?php
+<?php // This file deletes a product or service from the database.
 include 'DBConnect.php';
 
+// Check if the form has been submitted
 if (isset($_POST["item_id"]) && isset($_POST["business_id"])) {
+    // Get the item_id and business_id from the form
     $itemID = $_POST["item_id"];
     $businessID = $_POST["business_id"];
 
+    // Prepare the statement to delete the product from the database
     $stmt = $conn->prepare("DELETE FROM products_and_services WHERE item_id = ?");
     $stmt->bind_param("s", $itemID);
     $stmt->execute();
